dffe083a2b63b009b41c0cb6fed73cdafe1084f7
Atnabon <atnabond@gmail.com>
2022-12-14 18:54:33 +0300

    updated

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Filters from "./components/Filters";
+import Footer from "./components/Footer";
+import Rentals from "./components/Rentals";
+
+function App() {
+  return (
+    <div className="">
+      {/* Navbar */}
+      <Navbar />
+      {/* Filters */}
+      <div className="sm:mx-6 md:mx-10 lg:mx-12 px-3">
+        <Filters />
+        {/* Rentals */}
+        <Rentals />
+      </div>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+
